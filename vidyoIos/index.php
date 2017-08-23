b2e463c09a1330f2da695fa4ae73a72532e68961
marwaybs <bsmarway@gmail.com>
2017-08-23 15:28:50 -0400

    removed quotes around $token

--- vidyoIos/index.php
+++ vidyoIos/index.php
@@ -4,6 +4,7 @@ $command = escapeshellcmd('python3 generateToken.py --key=rUlaMASgt1Byi4Kp3sKYDe
 $token = shell_exec($command);
 // <a href="inkblottherapy://?token="$token"&resourceId=123&host=123&hideConfig=1">Open ios app</a>
 
-echo "<a href=VidyoConnector://?token='$token'&resourceId=blueHarp&host=prod.vidyo.io&hideConfig=1>Open ios app</a>"
+echo "<a href=VidyoConnector://?token=$token&resourceId=blueHarp&host=prod.vidyo.io&hideConfig=1>Open ios app</a>"
+
 
 ?>
